932f5ce800cde7aea24db9e9c5af3927d4b19631
Mistress_Meeple <jskynol001@gmail.com>
2021-05-21 17:35:48 +0100

    Create cloudflare-api.sh

---
+++ ddns-cloudflare/cloudflare-api.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+function update_record_name_env() { 
+    local RESULT=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$A_RECORD_ID" \
+        -H "content-type: application/json" \
+        -H "Authorization: Bearer $API_TOKEN"
+    )
+    A_RECORD_NAME=$(echo "$RESULT" | jq -r .result[0].name) 
+    echo "Updated A_RECORD_NAME: $A_RECORD_NAME"
+}
+
+function update_record_ID_env(){
+    local RESULT=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A$name=$A_RECORD_NAME" \
+        -H "content-type: application/json" \
+    	-H "Authorization: Bearer $API_TOKEN"
+    )
+	A_RECORD_ID=$(echo "$RESULT" | jq -r .result[0].id)
+}
+function update_record_env() {
+
+    # If ID is not set then pull by name
+    if [ ! -z "$A_RECORD_ID"  &&  -z "$A_RECORD_NAME" ]; then
+        update_record_name_env
+    #If Name is not set then pull by ID
+    elif [  -z "$A_RECORD_ID"  && ! -z "$A_RECORD_NAME" ]; then
+        update_record_ID_env
+    fi
+}
+function update_cached_ip() {
+
+    local RESULT=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$A_RECORD_ID" \
+        -H "content-type: application/json" \
+        -H "Authorization: Bearer $API_TOKEN"
+    )
+    CACHED_IP_RECORD=$(echo "$RESULT" | jq -r .result[0].content)
+}
